d9b8ccac480a4915f779d10eae0808eefaceeb92
kornxz <74502755+kornxz@users.noreply.github.com>
2022-05-25 10:44:47 +0700

    Update index.js

--- source/index.js
+++ source/index.js
@@ -1,10 +1,9 @@
 const { Client, Collection } = require("discord.js");
-const keepAlive = require("./server");
 const client = new Client({
     intents: 32767,
 });
 module.exports = client;
-require('discord-modals')(client)
+
 // Global Variables
 client.commands = new Collection();
 client.slashCommands = new Collection();
@@ -14,5 +13,4 @@ client.config = require("./config.json");
 
 // Initializing the project
 require("./handler")(client);
-keepAlive()
 client.login(client.config.token);
